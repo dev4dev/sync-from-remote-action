236fa823492680c5208b7a5baf857eee4e95d364
Alex Antonyuk <alex@antonyuk.me>
2023-03-10 19:40:06 +0200

    grep

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,7 +3,7 @@ import * as exec from '@actions/exec'
 
 export async function getRemoteVersion(repo: string): Promise<string> {
   return execPromise(
-    `git ls-remote --tags --sort="-v:refname" ${repo} | grep -P "refs/tags/v?\d+.\d+.\d+$"` //| head -n 1 | awk "{print $2}" | awk -F/ "{print $NF}"`
+    `git ls-remote --tags --sort="-v:refname" ${repo} | grep -P "refs/tags/"` //| head -n 1 | awk "{print $2}" | awk -F/ "{print $NF}"`
   )
 }
 
