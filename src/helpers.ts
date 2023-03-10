aa09f5b84cdfd8f240965f49b5295107701a0f5c
Alex Antonyuk <alex@antonyuk.me>
2023-03-10 18:48:04 +0200

    check version

---
+++ src/helpers.ts
@@ -0,0 +1,26 @@
+import * as exec from '@actions/exec'
+
+export async function getRemoteVersion(repo: string): Promise<string> {
+  return execPromise(
+    `git ls-remote --tags --sort='-v:refname' ${repo} | grep -P "refs/tags/v?\\d+.\\d+.\\d+$" | head -n 1 | awk '{print $2}' | awk -F/ '{print $NF}'`
+  )
+}
+
+export function getLocalVersion(): string {
+  return ''
+}
+
+async function execPromise(command: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    exec.exec(command, [], {
+      listeners: {
+        stdout: data => {
+          resolve(data.toString())
+        },
+        stderr: data => {
+          reject(data.toString())
+        }
+      }
+    })
+  })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,40 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 // import * as exec from '@actions/exec'
+import {getRemoteVersion} from './helpers'
 
 async function run(): Promise<void> {
   try {
     const source: string = core.getInput('source')
+    const githubToken = process.env['GITHUB_TOKEN'] as string
+    const octokit = github.getOctokit(githubToken)
 
-    core.startGroup('Test Group')
-    core.info(`Syncing with the remote repo ${source}`)
-    core.info(github.context.repo.repo)
-    core.endGroup()
+    core.info(`${octokit}, ${source}`)
+
+    // 0. Clone current repo
+
+    // 1. Get remote version
+    const remoteVersion = await getRemoteVersion(source)
+    core.info(`Remote version: ${remoteVersion}`)
+
+    // 2. Get local version
+
+    // 3. Compare version
+
+    // 4. SYNC IF NEEDED
+
+    // 5. Clone remote repo
+
+    // 6. Delete local files (except .github, .git, ...?)
+
+    // 7. Copy remote files (except .github, .git)
+
+    // 8. git add --all && git commit with version name && git push
+
+    // core.startGroup('Test Group')
+    // core.info(`Syncing with the remote repo ${source}`)
+    // core.info(github.context.repo.repo)
+    // core.endGroup()
 
     core.setOutput('result', 'synced')
   } catch (error) {
